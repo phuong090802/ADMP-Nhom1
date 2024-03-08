384c577c542e93bad813cd5091d3fbdd7c909fa6
phuong090802 <20110547@student.hcmute.edu.vn>
2024-03-08 16:14:31 +0700

    Update template

---
+++ client/App.js
@@ -0,0 +1,23 @@
+import 'react-native-get-random-values';
+import Realm from 'realm';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log(new Realm.BSON.ObjectId());
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
